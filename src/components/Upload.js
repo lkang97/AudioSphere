2a981e143f4ae7211bb9fca5bea965f608581f08
Lisa Kang <lleekkang@gmail.com>
2020-06-23 23:12:52 -0400

    Add image preview to song upload form

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -6,6 +6,11 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import FormControl from "@material-ui/core/FormControl";
+import IconButton from "@material-ui/core/IconButton";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
+import ImageIcon from "@material-ui/icons/Image";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
 
 import "../styles/upload.css";
 
@@ -15,8 +20,10 @@ const Upload = () => {
   const [songTitle, setSongTitle] = useState(null);
   const [songGenre, setSongGenre] = useState(null);
   const [songDesc, setSongDesc] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [file, setFile] = useState();
 
-  const uploadFile = async (e) => {
+  const uploadAudio = async (e) => {
     const formData = new FormData();
     formData.append("file", e.target.files[0]);
 
@@ -35,6 +42,26 @@ const Upload = () => {
     }
   };
 
+  const handleImage = async (e) => {
+    const formData = new FormData();
+    formData.append("file", e.target.files[0]);
+    setFile(e.target.files[0]);
+
+    // const token = await getTokenSilently();
+
+    // const response = await fetch(`${api}/upload`, {
+    //   method: "POST",
+    //   headers: {
+    //     Authorization: `Bearer ${token}`,
+    //   },
+    //   body: formData,
+    // });
+    // if (response.ok) {
+    //   const imgUrl = await response.json();
+    //   setImageUrl(imgUrl);
+    // }
+  };
+
   const handleTitle = async (e) => {
     setSongTitle(e.target.value);
   };
@@ -51,21 +78,44 @@ const Upload = () => {
     <div className="upload-container">
       <div className="dnd-container">
         <h2>Drag and drop your audio file here</h2>
-        <form>
+        <form className="upload-form">
           <FormControl>
             <Button className="upload-button" variant="contained">
               <input
                 accept="audio/*"
                 style={{ display: "none" }}
                 type="file"
-                id="raised-button-file"
-                onChange={uploadFile}
+                id="raised-button-audio"
+                onChange={uploadAudio}
               ></input>
-              <label htmlFor="raised-button-file">
+              <label htmlFor="raised-button-audio">
                 or click here to upload.
               </label>
             </Button>
           </FormControl>
+          <div>
+            <Card>
+              {file ? (
+                <CardMedia>
+                  <img src={URL.createObjectURL(file)} alt="preview" />
+                </CardMedia>
+              ) : (
+                <ImageIcon color="primary" />
+              )}
+            </Card>
+            <IconButton>
+              <input
+                accept="image/*"
+                style={{ display: "none" }}
+                type="file"
+                id="raised-button-file"
+                onChange={handleImage}
+              ></input>
+              <label htmlFor="raised-button-file">
+                <AddAPhotoIcon variant="raised"></AddAPhotoIcon>
+              </label>
+            </IconButton>
+          </div>
           {/* {songUrl ? ( */}
           <div className="upload-song-details">
             <FormControl>
