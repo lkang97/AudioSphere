1c5a5849f1c079c1ea5fb56894f4fe5f2060a068
Lisa Kang <lleekkang@gmail.com>
2020-06-24 23:25:08 -0400

    Add volume slider to music player component

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -7,11 +7,15 @@ import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
 import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
 import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
 import SkipNextIcon from "@material-ui/icons/SkipNext";
-import VolumeUpIcon from "@material-ui/icons/VolumeUp";
 import VolumeOffIcon from "@material-ui/icons/VolumeOff";
 import IconButton from "@material-ui/core/IconButton";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import Slider from "@material-ui/core/Slider";
+import VolumeDown from "@material-ui/icons/VolumeDown";
+import VolumeUp from "@material-ui/icons/VolumeUp";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -22,6 +26,11 @@ const useStyles = makeStyles((theme) => ({
   appBar: {
     top: "auto",
     bottom: 0,
+    backgroundColor: "gray",
+  },
+  toolBar: {
+    display: "flex",
+    justifyContent: "space-between",
   },
 }));
 
@@ -32,13 +41,17 @@ const MusicPlayer = () => {
   const currentSong = useSelector((state) => state.currentSong);
   const [duration, setDuration] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [volume, setVolume] = useState(0.5);
+  const [volume, setVolume] = useState(30);
   const [mute, setMute] = useState(false);
+
+  const handleVolChange = (event, newValue) => {
+    setVolume(newValue);
+  };
 
   return (
     <div className="song-bar-container">
       <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
+        <Toolbar className={classes.toolBar}>
           <div className="song-info"></div>
           <div className="song-actions">
             <IconButton>
@@ -55,7 +68,23 @@ const MusicPlayer = () => {
               <SkipNextIcon />
             </IconButton>
           </div>
-          <div className="song-volume"></div>
+          <div id="song-volume">
+            <Grid container spacing={2}>
+              <Grid item>
+                <VolumeDown />
+              </Grid>
+              <Grid item xs>
+                <Slider
+                  value={volume}
+                  onChange={handleVolChange}
+                  aria-labelledby="continuous-slider"
+                />
+              </Grid>
+              <Grid item>
+                <VolumeUp />
+              </Grid>
+            </Grid>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
