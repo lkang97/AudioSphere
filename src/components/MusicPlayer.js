3231af265013180f2218ffe124a0a27466f0ae82
Lisa Kang <lleekkang@gmail.com>
2020-06-26 16:55:55 -0400

    Got audio to play with custom buttons

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setCurrentSong } from "../store/state";
 import "../styles/music-player.css";
@@ -45,7 +45,7 @@ const useStyles = makeStyles((theme) => ({
 
 const MusicPlayer = () => {
   const classes = useStyles();
-
+  const interval = useRef(null);
   const dispatch = useDispatch();
   const currentSong = useSelector((state) => state.song);
   const [duration, setDuration] = useState(0);
@@ -63,82 +63,116 @@ const MusicPlayer = () => {
   };
 
   const handleMetadata = (e) => {
-    const audio = document.getElementById("audio");
+    let audio = document.getElementById("audio");
     setDuration(audio.duration);
   };
 
-  const handleOnPlaying = () => {};
+  const handleOnPlaying = (e) => {
+    let audio = document.getElementById("audio");
+    if (!audio.paused) {
+      interval.current = setInterval(() => {
+        setCurrTime(audio.currentTime);
+      }, 50);
+    }
+  };
+  const handlePlayPause = () => {
+    let audio = document.getElementById("audio");
+
+    if (isPlaying) {
+      audio.pause();
+      clearInterval(interval.current);
+      setIsPlaying(false);
+    } else {
+      audio.play();
+      setIsPlaying(true);
+    }
+  };
 
   const handleOnEnded = () => {};
 
-  return (
-    <div className="song-bar-container">
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar className={classes.toolBar}>
+  const musicPlayer = (
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar className={classes.toolBar}>
+        <div>
+          {currentSong ? (
+            <div className="song-info">
+              <div className="song-title">{currentSong.title}</div>
+              <div className="song-user">{currentSong.user.nickname}</div>
+            </div>
+          ) : (
+            <div className="song-info"></div>
+          )}
+        </div>
+        <div className="song-actions">
           <div>
+            <IconButton className={classes.buttons}>
+              <SkipPreviousIcon />
+            </IconButton>
+            <IconButton className={classes.buttons} onClick={handlePlayPause}>
+              {isPlaying ? (
+                <PauseCircleOutlineIcon className={classes.playBtn} />
+              ) : (
+                <PlayCircleOutlineIcon className={classes.playBtn} />
+              )}
+            </IconButton>
+            <IconButton className={classes.buttons}>
+              <SkipNextIcon />
+            </IconButton>
+          </div>
+          <div className="song-slider">
             {currentSong ? (
-              <div className="song-info">
-                <div className="song-title">{currentSong.title}</div>
-                <div className="song-user">{currentSong.user.nickname}</div>
-              </div>
+              <div className="song-duration">{currTime}</div>
             ) : (
-              <div className="song-info"></div>
+              <div className="song-duration">--.--</div>
+            )}
+            <Slider
+              value={currTime}
+              onChange={handleSongSlide}
+              max={duration}
+            />
+            {currentSong ? (
+              <div className="song-duration">{duration}</div>
+            ) : (
+              <div className="song-duration">--.--</div>
             )}
           </div>
-          <div className="song-actions">
-            <div>
-              <IconButton className={classes.buttons}>
-                <SkipPreviousIcon />
-              </IconButton>
-              <IconButton className={classes.buttons}>
-                {isPlaying ? (
-                  <PauseCircleOutlineIcon className={classes.playBtn} />
-                ) : (
-                  <PlayCircleOutlineIcon className={classes.playBtn} />
-                )}
-              </IconButton>
-              <IconButton className={classes.buttons}>
-                <SkipNextIcon />
-              </IconButton>
-            </div>
-            <div className="song-slider">
-              <Slider value={duration} onChange={handleSongSlide} />
-              {currentSong ? (
-                <div className="song-duration">{duration}</div>
-              ) : (
-                <div className="song-duration">--.--</div>
-              )}
-            </div>
-          </div>
-          <div id="song-volume">
-            <Grid container spacing={2}>
-              <Grid item>
-                <VolumeDown />
-              </Grid>
-              <Grid item xs>
-                <Slider
-                  value={volume}
-                  onChange={handleVolChange}
-                  aria-labelledby="continuous-slider"
-                />
-              </Grid>
-              <Grid item>
-                <VolumeUp />
-              </Grid>
+        </div>
+        <div id="song-volume">
+          <Grid container spacing={2}>
+            <Grid item>
+              <VolumeDown />
             </Grid>
-          </div>
-        </Toolbar>
-      </AppBar>
+            <Grid item xs>
+              <Slider
+                value={volume}
+                onChange={handleVolChange}
+                aria-labelledby="continuous-slider"
+              />
+            </Grid>
+            <Grid item>
+              <VolumeUp />
+            </Grid>
+          </Grid>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+
+  return (
+    <div className="song-bar-container">
       {currentSong ? (
-        <audio
-          id="audio"
-          src={currentSong.song_url}
-          controls
-          controlsList="nodownload"
-          onLoadedMetadata={handleMetadata}
-          onPlaying={handleOnPlaying}
-          onEnded={handleOnEnded}
-        />
+        <>
+          <audio
+            id="audio"
+            controls
+            src={currentSong.song_url}
+            // src="https://audiosphere-project.s3.us-east-2.amazonaws.com/01_Gallery.mp3"
+            onLoadedMetadata={handleMetadata}
+            onPlaying={handleOnPlaying}
+            onEnded={handleOnEnded}
+          />
+          {musicPlayer}
+        </>
       ) : (
         <div></div>
       )}
